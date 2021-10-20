0f8c927968a39c9df355b0d0e911a53e48b34741
Jorge Luis YH <jorgeluisyh@gmail.com>
2021-10-20 15:30:38 -0500

    agregado el script para crear el output y agregar los archivos dede el repositorio de origen

--- Generales/scripts/pushtoelbromercio.py
+++ Generales/scripts/pushtoelbromercio.py
@@ -2,6 +2,18 @@ import os
 import shutil
 
 
+ruta_output = "output"
+dirgenerales = os.path.dirname(os.path.dirname(__file__))
+scriptsdir = os.path.join(dirgenerales, 'scripts')
+sourcesdir = os.path.join(dirgenerales, 'sources')
+
+def creamos_output():
+    """
+    Necesitamos crear el output, ya que es necesario por el repositorio cpina/github-action-push-to-another-repository@main
+    se requiere que se cree una carpeta y a partir de ella se envíen los archivos
+    """
+    os.mkdir("output")
+
 
 def copytree(src, dst, symlinks= False, ignore=None):
     for item in os.listdir(src):
@@ -12,3 +24,16 @@ def copytree(src, dst, symlinks= False, ignore=None):
         else:
             if not os.path.exists(d) or os.stat(s).st_mtime - os.stat(d).st_mtime > 1:
                 shutil.copy2(s, d)
+
+def main():
+    creamos_output()
+    src_path = os.path.join(sourcesdir, 'elBromercio')
+    dst_path = ruta_output
+    # Copiamos los archivos del repositorio a la ruta output
+    copytree(src_path, dst_path)
+
+
+if __name__ == '__main__':
+    print(scriptsdir)
+    print(sourcesdir)
+    main()
