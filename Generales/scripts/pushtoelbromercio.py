e4be732df002da1ebf6e47372f8e73591c4a3eb9
Jorge Luis YH <jorgeluisyh@gmail.com>
2021-10-20 14:26:03 -0500

    agregados valores generales para elbromercio

---
+++ Generales/scripts/pushtoelbromercio.py
@@ -0,0 +1,14 @@
+import os
+import shutil
+
+
+
+def copytree(src, dst, symlinks= False, ignore=None):
+    for item in os.listdir(src):
+        s = os.path.join(src, item)
+        d = os.path.join(dst, item)
+        if os.path.isdir(s):
+            shutil.copytree(s, d, symlinks, ignore)
+        else:
+            if not os.path.exists(d) or os.stat(s).st_mtime - os.stat(d).st_mtime > 1:
+                shutil.copy2(s, d)
